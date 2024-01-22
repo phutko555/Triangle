ef8c9bb5cb247cd9be41d17c1962d87be3bf4c18
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-22 13:39:50 +0000

    Update Triangle.java

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -1,17 +1,23 @@
 package com.epam.rd.autotasks.triangle;
-
 class Triangle {
-
+    private Point a, b, c;
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+        if (isDegenerative(a, b, c)) {
+            throw new IllegalArgumentException("Degenerative triangle, please provide valid vertices.");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
-
+    private boolean isDegenerative(Point a, Point b, Point c) {
+        return (b.y - a.y) * (c.x - b.x) == (c.y - b.y) * (b.x - a.x);
+    }
     public double area() {
-        //TODO
+        return 0.5 * Math.abs((a.x - c.x) * (b.y - a.y) - (a.x - b.x) * (c.y - a.y));
     }
-
-    public Point centroid(){
-        //TODO
+    public Point centroid() {
+        double centroidX = (a.x + b.x + c.x) / 3.0;
+        double centroidY = (a.y + b.y + c.y) / 3.0;
+        return new Point(centroidX, centroidY);
     }
-
 }
